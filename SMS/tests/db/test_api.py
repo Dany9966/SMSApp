9e8281e8ec8e07d7e362e8417eec546092fc6b7a
Dany9966 <vincze.daniel96@gmail.com>
2018-07-12 15:50:02 +0300

    Update test_api.py

--- SMS/tests/db/test_api.py
+++ SMS/tests/db/test_api.py
@@ -1,20 +1,17 @@
 import unittest
-from mock import patch, Mock
+from mock import patch
 import SMS.db.api as db_api
 
 
 class TestAPI(unittest.TestCase):
-    def test_add_user(self):
+    @patch('SMS.db.models.User')
+    @patch('SMS.db.session.ensure_session')
+    def test_add_user(self, user_mock, session_mock):
         # Mocking User object
-        with patch('SMS.db.models.User', return_value=Mock()) as user_mock:
-            user_mock.save.return_value = '1'
+        user_mock.return_value.save.return_value = '1'
 
-            ret = db_api.add_user(name='someuser', session=None)
-            # assertions
-            user_mock.assert_called_once_with('someuser')
-            user_mock.save.assert_called_once()
-            self.assertEqual(ret, '1')
-
-
-if __name__ == '__main__':
-    unittest.main()
+        ret = db_api.add_user(name='someuser', session=session_mock)
+        # assertions
+        user_mock.assert_called_once_with('someuser')
+        user_mock.return_value.save.assert_called_once()
+        self.assertEqual('1', ret)
