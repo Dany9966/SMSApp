aabb5373e33e9a0ab785625a2504f85380c3c65e
Dany9966 <vincze.daniel96@gmail.com>
2018-07-21 12:51:42 +0300

    fixed apt-install script

--- SMS/tools/apt-install.sh
+++ SMS/tools/apt-install.sh
@@ -1,6 +1,8 @@
-sudo apt-get install python python-dev python-pip rabbitmq-server
+script_path=`dirname $0`
+cd $script_path/../..
+sudo apt-get install -y python python-dev python-pip rabbitmq-server
 sudo -H pip install --upgrade pip
 sudo -H pip install setuptools
 sudo rabbitmqctl add_user stackrabbit Passw0rd
 sudo rabbitmqctl set_permissions stackrabbit ".*" ".*" ".*"
-cd ../.. ; sudo -H pip install .
+sudo -H pip install -e .
