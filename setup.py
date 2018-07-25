ef9cdf4e5f85a47ed10773e15754669d3db782a6
Dany9966 <vincze.daniel96@gmail.com>
2018-07-25 13:52:18 +0300

    - windows deploy script THEORETICALLY ready to roll (back to completing linux script now)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="SMS",
@@ -8,7 +8,7 @@ setup(
 Supercalifragilisticexpialidocius Monitoring System (SMS) that monitors client
 system resource usage (i.e. CPU percentage) and stores data on server app
 """,
-    packages=['SMS'],
+    packages=find_packages(),
     install_requires=['pika', 'sqlalchemy', 'psutil'],
     entry_points={
         'console_scripts': [
